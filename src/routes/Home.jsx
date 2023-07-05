e19bdeff9a5841dc5aa06dd0a3eb780ae61c46ed
NoOneLevi <zxvitor1@hotmail.com>
2023-07-05 18:46:55 -0300

    Change the head and install helmet

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,12 +3,16 @@ import { BiSearchAlt2 } from 'react-icons/bi'
 import { AiOutlineHome } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import '../styles/Home.css'
+import { Helmet } from "react-helmet"
 
 export default function Home() {
     const { id } = JSON.parse(localStorage.getItem('user'));
 
     return (
         <div className="container-home">
+            <Helmet>
+                <title>Levi - Clone - Strawberries</title>
+            </Helmet>
             <nav className="nav-bar">
                 <ul className="ul-home">
                     <li className="li-home">
